a7240a77ea8a0cdc12272438b71bf34f72a74051
Alberto Rodriguez Garcia <agarcia@schubergphilis.com>
2020-02-28 23:21:33 +0100

    updates azurerm provider

--- version.tf
+++ version.tf
@@ -3,5 +3,10 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "=1.44.0"
+  version         = "=2.0.0"
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
+  subscription_id = var.SUBSCRIPTION_ID
+  features {}
 }
